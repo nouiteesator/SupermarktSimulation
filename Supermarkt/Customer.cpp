e651c1295105aad50109b80440714ccb6f8e3ef3
chris <nouiteesator@gmx.de>
2017-06-06 11:38:28 +0200

    started to implement functions not ready yet

--- Supermarkt/Customer.cpp
+++ Supermarkt/Customer.cpp
@@ -14,13 +14,17 @@ Customer::~Customer()
 
 Customer::Customer(simpleTime arrival){
 	arrivalTime = arrival;
+	itemAmount = generateItemAmount();
 }
 
 //TODO get seed
 int Customer::generateItemAmount(){
 	//int seed =getSeed
 	//poisson distribution 	
+	//TODO implement poisson
 }
+
+
 
 
 /*
